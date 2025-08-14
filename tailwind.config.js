643cbdc2cb086688f9af72d035fa927899356fea
frantan <francinetaing13@gmail.com>
2025-08-14 17:30:41 +0700

    setup Tailwind, liver-server

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        
+      }
+    },
   },
   plugins: [],
 }
-
