baeca98706c30fdff631f94f5b85dfe68de52408
frantan <francinetaing13@gmail.com>
2025-08-14 18:10:32 +0700

    + presets setup; tried out tailwind, works!

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ module.exports = {
         // Colors
         background: '#0D0D0D',
         foreground: '#161616',
-        textSecondary: '#4E4E4E',
-        textPrimary: '#FFFFFF',
-        accent: '#951313',
       },
       // Typography!
       // Font Family
@@ -17,17 +14,24 @@ module.exports = {
         heading: ['Gloock', 'serif'],   // All Headings
         body: ['Geist Mono', 'monospace'], // Body Text
       },
+      // Font Colors
+      textColor: {
+        textSecondary: '#4E4E4E',
+        textPrimary: '#FFFFFF',
+        accent: '#951313',
+      },
       // Font Sizes
       fontSize: {
-        // Gloock Heading Sizes
-        headingXL: ['128px', { lineHeight: '140px' }],
-        headingL: ['64px', { lineHeight: '72px' }],
-        headingM: ['27px', { lineHeight: '32px' }],
-        // Geist Mono Body Sizes
-        bodyL: ['27px', { lineHeight: '32px' }],
-        bodyM: ['19px', { lineHeight: '24px' }],
-        bodyS: ['16px', { lineHeight: '20px' }],
-      }
+        // Geist Mono (body / headers)
+        body: ['1.6875rem', { lineHeight: '2rem' }], // 27px → 32px
+        header: ['1.1875rem', { lineHeight: '1.5rem' }], // 19px → 24px
+        version: ['1rem', { lineHeight: '1.25rem' }], // 16px → 20px
+
+        // Gloock (headings)
+        headingXL: ['8rem', { lineHeight: '9rem' }],   // 128px → 144px
+        headingL: ['4rem', { lineHeight: '4.5rem' }], // 64px → 72px
+        headingM: ['1.6875rem', { lineHeight: '2rem' }], // 27px → 32px
+      },
     },
   },
   plugins: [],
