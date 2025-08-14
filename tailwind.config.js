8dc40cb45d6bc34b29d081bfb33bb5411259a4dc
frantan <francinetaing13@gmail.com>
2025-08-14 17:52:40 +0700

    + more setup: colors/fonts/sizes; some tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-
+        // Colors
+        background: '#0D0D0D',
+        foreground: '#161616',
+        textSecondary: '#4E4E4E',
+        textPrimary: '#FFFFFF',
+        accent: '#951313',
       },
+      // Typography!
+      // Font Family
       fontFamily: {
-        
+        heading: ['Gloock', 'serif'],   // All Headings
+        body: ['Geist Mono', 'monospace'], // Body Text
+      },
+      // Font Sizes
+      fontSize: {
+        // Gloock Heading Sizes
+        headingXL: ['128px', { lineHeight: '140px' }],
+        headingL: ['64px', { lineHeight: '72px' }],
+        headingM: ['27px', { lineHeight: '32px' }],
+        // Geist Mono Body Sizes
+        bodyL: ['27px', { lineHeight: '32px' }],
+        bodyM: ['19px', { lineHeight: '24px' }],
+        bodyS: ['16px', { lineHeight: '20px' }],
       }
     },
   },
