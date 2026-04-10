d255eb4448b4c59346430a98abe2a9bcff6410a9
frantan <francinetaing13@gmail.com>
2026-04-10 21:23:20 +0700

    feat: change base path in vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: '/schrecknet-lite/', 
+  base: '/',
   build: {
     rollupOptions: {
       input: {
-        // Relative to project roo
         main: resolve(__dirname, 'index.html'),
         v20: resolve(__dirname, 'v20.html'),
         v5: resolve(__dirname, 'v5.html'),
